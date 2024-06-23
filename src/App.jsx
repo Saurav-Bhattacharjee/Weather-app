3a4e34661c0167a8bfd548d60174def3cc5bacfe
Saurav Bhattacharjee <saroh2009@gmail.com>
2024-06-23 19:40:41 +0530

    main app.js updated

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     const fetchWeather = async(city)=>{
       const apiKey = "c1edb1984e6b19880bf4a3dfd5e1c4f1";
 
-      const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric');
+      const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}');
       const data = await response.json();
       setWeather(data);
     }
