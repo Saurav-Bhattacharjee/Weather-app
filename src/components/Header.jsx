004264f29b242d69655ca2270a0b33996c94b3e1
Saurav Bhattacharjee <saroh2009@gmail.com>
2024-06-23 16:50:37 +0530

    Header done

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export const Header=()=>{
+    return (<div>
+        <div className="bg-gray-500 text-white w-full h-20 p-4 flex items-center" >
+           
+           <div className="container mx-auto flex justify-around items-center">
+                <div className="text-2xl font-bold"> Weather App </div>
+                <div className="text-sm">Check your local weather</div>
+           </div> 
+
+        </div>
+    </div>)
+}
